cb47329a6fb21ac3c2f69ef0150b67df977f3fc8
Max Base <MaxBaseCode@Gmail.Com>
2021-06-09 03:45:40 +0430

    Create verification.py

---
+++ tests/verification.py
@@ -0,0 +1,41 @@
+http://localhost:3003/v2/verification/
+{"authID": "79009322-59c3-45a6-b0da-c30632e97331", "verifyCode": "1010"}
+
+# {
+#     "status": 0,
+#     "alert": {
+#         "has": 1,
+#         "hide": 1,
+#         "title": "فعال سازی حساب",
+#         "message": "کد وارد شده معتبر نیست."
+#     },
+#     "result": []
+# }
+
+http://localhost:3003/v2/verification/
+{"authID": "79009322-59c3-45a6-b0da-c30632e97331", "verifyCode": "255"}
+
+# {
+#     "status": 1,
+#     "alert": {
+#         "has": 1,
+#         "hide": 1,
+#         "title": "",
+#         "message": "به میرسه خوش آمدید."
+#     },
+#     "result": {
+#         "sesionID": "a6746fd0-76d1-44be-8ed8-c86ccb159e85"
+#     }
+# }
+
+# after 10 times!
+
+# {
+#     "status": 0,
+#     "alert": {
+#         "has": 1,
+#         "title": "خطای سرور",
+#         "message": "شما تعداد زیادی کد اشتباه وارد کردید و این نشست باطل شد لطفا خارج شوید و مجدد تلاش کنید!"
+#     },
+#     "result": []
+# }
